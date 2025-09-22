2d32adb6db445a0dbdeca5ac45998de3b874cae4
FDfsdfhdf <87785247+FDfsdfhdf@users.noreply.github.com>
2025-09-21 21:55:03 -0300

    feat(ui): implement dark template home and sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
+	siteUrl: process.env.SITE_URL || 'https://manualdolojista.vercel.app',
+	generateRobotsTxt: true,
+	outDir: 'out',
+};
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
 	siteUrl: 'https://manualdolojista.vercel.app',
 	generateRobotsTxt: true,
 	changefreq: 'weekly',
